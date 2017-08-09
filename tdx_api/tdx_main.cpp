d82affc132e95a5d1c0f65022f086214fc951fdd
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2017-08-09 16:22:46 +0800

    把返回值由dict改为list

--- tdx_api/tdx_main.cpp
+++ tdx_api/tdx_main.cpp
@@ -14,8 +14,8 @@
 
 using namespace std;
 
-boost::python::dict format(vector<tdx_data>& datas, vector<tdx_error>& errors) {
-	boost::python::dict rt;
+boost::python::list format(vector<tdx_data>& datas, vector<tdx_error>& errors) {
+	boost::python::list rt;
 	boost::python::list dt, er;
 	for (auto& item : datas) {
 		dt.append(item.string());
@@ -25,8 +25,8 @@ boost::python::dict format(vector<tdx_data>& datas, vector<tdx_error>& errors) {
 		er.append(item.string());
 	}
 
-	rt["data"] = dt;
-	rt["errInfo"] = er;
+	rt.append(dt);
+	rt.append(er);
 
 	return rt;
 }
@@ -37,35 +37,40 @@ struct TdxApi {
 		instance.Init(dll);
 	}
 	
-	int login(char* IP, short Port, char* Version, short YybID, char* AccountNo, char* TradeAccount, char* JyPassword, char* TxPassword, char* ErrInfo) {
-		return instance.Logon(IP, Port, Version, YybID, AccountNo, TradeAccount, JyPassword, TxPassword, ErrInfo);
+	boost::python::list login(char* IP, short Port, char* Version, short YybID, char* AccountNo, char* TradeAccount, char* JyPassword, char* TxPassword) {
+		tdx_error ErrInfo;
+		boost::python::list rt;
+		int clientID = instance.Logon(IP, Port, Version, YybID, AccountNo, TradeAccount, JyPassword, TxPassword, ErrInfo);
+		rt.append(clientID);
+		rt.append(ErrInfo.string());
+		return rt;
 	}
 
 	void logoff(int ClientID) {
 		instance.Logoff(ClientID);
 	}
 
-	boost::python::dict QueryHistoryData(int ClientID, int Category, char* StartDate, char* EndDate) {
+	boost::python::list QueryHistoryData(int ClientID, int Category, char* StartDate, char* EndDate) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.QueryHistoryData(ClientID, Category, StartDate, EndDate, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 
-	boost::python::dict QueryData(int ClientID, int Category) {
+	boost::python::list QueryData(int ClientID, int Category) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.QueryData(ClientID, Category, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 
-	boost::python::dict QueryDatas(int ClientID, boost::python::list Category) {
+	boost::python::list QueryDatas(int ClientID, boost::python::list Category) {
 		std::vector<int> cats = from_python_list<int>(Category);
 		int size = cats.size();
 		vector<tdx_data> datas(size);
@@ -75,17 +80,17 @@ struct TdxApi {
 		return format(datas,errors);
 	}
 
-	boost::python::dict SendOrder(int ClientID, int Category, int PriceType, char* Gddm, char* Zqdm, float Price, int Quantity) {
+	boost::python::list SendOrder(int ClientID, int Category, int PriceType, char* Gddm, char* Zqdm, float Price, int Quantity) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.SendOrder(ClientID, Category, PriceType, Gddm, Zqdm, Price, Quantity, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 
-	boost::python::dict SendOrders(int ClientID,
+	boost::python::list SendOrders(int ClientID,
 		boost::python::list Category,
 		boost::python::list PriceType,
 		boost::python::list Gddm,
@@ -108,17 +113,17 @@ struct TdxApi {
 		return format(datas,errors);
 	}
 
-	boost::python::dict CancelOrder(int ClientID, char* ExchangeID, char* hth) {
+	boost::python::list CancelOrder(int ClientID, char* ExchangeID, char* hth) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.CancelOrder(ClientID, ExchangeID, hth, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 
-	boost::python::dict CancelOrders(int ClientID,
+	boost::python::list CancelOrders(int ClientID,
 		boost::python::list ExchangeID,
 		boost::python::list hth){
 		vector<char*> exchange_array = from_python_list<char*>(ExchangeID);
@@ -130,17 +135,17 @@ struct TdxApi {
 		return format(datas,errors);
 	}
 
-	boost::python::dict GetQuote(int ClientID, char* Zqdm) {
+	boost::python::list GetQuote(int ClientID, char* Zqdm) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.GetQuote(ClientID, Zqdm, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 
-	boost::python::dict GetQuotes(int ClientID, boost::python::list Zqdm) {
+	boost::python::list GetQuotes(int ClientID, boost::python::list Zqdm) {
 		vector<char*> zqdm_array = from_python_list<char*>(Zqdm);
 		int size = zqdm_array.size();
 		vector<tdx_data> datas(size);
@@ -149,13 +154,13 @@ struct TdxApi {
 		return format(datas, errors);
 	}
 
-	boost::python::dict Repay(int ClientID, char* Amount) {
+	boost::python::list Repay(int ClientID, char* Amount) {
 		tdx_data Result;
 		tdx_error ErrInfo;
 		instance.Repay(ClientID, Amount, Result, ErrInfo);
-		boost::python::dict rt;
-		rt["data"] = Result.string();
-		rt["errInfo"] = ErrInfo.string();
+		boost::python::list rt;
+		rt.append(Result.string());
+		rt.append(ErrInfo.string());
 		return rt;
 	}
 };
@@ -165,8 +170,8 @@ BOOST_PYTHON_MODULE(tdx_api)
 	using namespace boost::python;
 	class_<TdxApi>("TdxApi").
 		def("init", &TdxApi::init).
-		def("login", &TdxApi::login).
-		def("logoff", &TdxApi::logoff).
+		def("Logon", &TdxApi::login).
+		def("Logoff", &TdxApi::logoff).
 		def("QueryHistoryData", &TdxApi::QueryHistoryData).
 		def("QueryData", &TdxApi::QueryData).
 		def("QueryDatas", &TdxApi::QueryDatas).
