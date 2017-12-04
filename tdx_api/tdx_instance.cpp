b606b7714b72c613077264af0b03d6dc269a5a06
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2017-12-04 22:21:21 +0800

    输出错误信息

--- tdx_api/tdx_instance.cpp
+++ tdx_api/tdx_instance.cpp
@@ -1,14 +1,46 @@
 #include "stdafx.h"
 #include "tdx_instance.h"
 #include "consts.h"
-
+#include <iostream>
 #include <string>
 
 using namespace std;
 
+std::string GetLastErrorStdStr()
+{
+	DWORD error = GetLastError();
+	if (error)
+	{
+		LPVOID lpMsgBuf;
+		DWORD bufLen = FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			error,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0, NULL);
+		if (bufLen)
+		{
+			LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
+			std::string result(lpMsgStr, lpMsgStr + bufLen);
+
+			LocalFree(lpMsgBuf);
+
+			return result;
+		}
+	}
+	return std::string();
+}
+
 void TdxInstance::Open(string dll_name)
 {
 	module_ = LoadLibrary(dll_name.c_str());
+	if (module_ == NULL) {
+		cout <<"dll加载失败: '"<<dll_name<<"',"<<GetLastErrorStdStr()<< endl;
+		exit(-1);
+	}
 	//获取api函数
 	OpenTdx = (OpenTdxDelegate)GetProcAddress(module_, "OpenTdx");
 	CloseTdx = (CloseTdxDelegate)GetProcAddress(module_, "CloseTdx");
