4c766131a1389c02d71e23773ffa6f6dfb906378
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2018-05-04 10:29:02 +0800

    bugfix: print error

--- tdx_api/tdx_instance.cpp
+++ tdx_api/tdx_instance.cpp
@@ -1,14 +1,65 @@
 #include "stdafx.h"
 #include "tdx_instance.h"
 #include "consts.h"
+#include "Shlwapi.h"
+
+#include <windows.h>
+#include <strsafe.h>
 
 #include <string>
+#include <stdio.h>
 
-using namespace std;
+void print_error() {
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	// Display the error message and exit the process
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("failed with error %d: %s"),
+		dw, lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	ExitProcess(dw);
+}
 
 void TdxInstance::Open(string dll_name)
 {
 	module_ = LoadLibrary(dll_name.c_str());
+
+	if (module_ == NULL) {
+		print_error();
+		printf("failed to load dll %s\n", dll_name.c_str());
+		char pwd[MAX_PATH];
+		char path[MAX_PATH];
+		GetCurrentDirectory(MAX_PATH, pwd);
+		PathCombine((char*)path, (char*)pwd, (char*)dll_name.c_str());
+
+		module_ = LoadLibrary(path);
+		if(module_ == NULL){
+			printf("failed to load dll %s\n", path);
+			print_error();
+			exit(-1);
+		}
+
+	}
+	
 	//»ñÈ¡apiº¯Êý
 	OpenTdx = (OpenTdxDelegate)GetProcAddress(module_, "OpenTdx");
 	CloseTdx = (CloseTdxDelegate)GetProcAddress(module_, "CloseTdx");
